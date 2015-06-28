413cff1f34a9231a44a7dd5f15bced1c856d091f
Thiago Fernandes Massa <thiagown@gmail.com>
2015-06-28 11:36:30 +0200

    Added the backspace behavior, missing lots of refactorings

--- src/cljs/smartyper/keypress.cljs
+++ src/cljs/smartyper/keypress.cljs
@@ -2,10 +2,22 @@
 
 (def last-keypress (atom 0))
 
+(defn- keypress-handler [e]
+  (let [key-code (.-keyCode e)]
+    (swap! last-keypress (fn[] key-code))))
+
 (defn- keydown-handler [e]
-  (swap! last-keypress #(.fromCharCode js/String (.-keyCode e))))
+  (let [key-code (.-keyCode e)
+        character (.fromCharCode js/String key-code)]
+    (when (= key-code 8)
+      (swap! last-keypress (fn[] key-code))
+      (.preventDefault e))))
+
+(defn key-code-to-character [key-code]
+  (.fromCharCode js/String key-code))
 
 (defn- charcode-to-char [charcode])
 
 (defn hook-keypress-detection! []
-  (.addEventListener js/document "keypress" keydown-handler false))
+  (.addEventListener js/document "keydown" keydown-handler false)
+  (.addEventListener js/document "keypress" keypress-handler false))
--- src/cljs/smartyper/typing_area.cljs
+++ src/cljs/smartyper/typing_area.cljs
@@ -3,9 +3,11 @@
             [reagent.core :as reagent :refer [atom]]
             [reagent.session :as session]))
 
-(def wrong-character-maps (atom []))
 (def character-maps (atom []))
+(def typed-characters (atom []))
+(def wrong-characters-stack (atom []))
 (defrecord CharacterMap [id character typed])
+(defrecord TypedCharacter [character correct])
 
 (defn set-text [text]
   "Sets the text for the component"
@@ -17,9 +19,7 @@
   "Finds the next CharacterMap needed to be typed"
   (first (filter #(= (:typed %) false) characters)))
 
-(defn- update-wrong-typed-characters [])
-
-(defn- update-latest-character-map-as-typed []
+(defn- update-latest-character-map-as-typed! []
   "Updates the latest CharacterMap as typed"
   (let [characters @character-maps
         character-map-id (:id (next-character-id-to-type characters))]
@@ -27,13 +27,44 @@
       character-maps
       #(assoc-in characters [character-map-id :typed] true))))
 
-(defn- keypress-watcher [key a old-val new-val]
-  (let [cm @character-maps
+(defn- update-latest-character-map-as-not-typed! []
+  "Updates the latest CharacterMap as typed"
+  (let [characters @character-maps
+        character-map-id (dec (:id (next-character-id-to-type characters)))]
+    (when (>= character-map-id 0)
+      (swap! character-maps #(assoc-in characters [character-map-id :typed] false))
+      )))
+
+(defn- add-wrong-character-to-stack [character]
+  (swap! wrong-characters-stack #(conj @wrong-characters-stack character)))
+
+(defn- remove-last-characters-stack! []
+  (swap! wrong-characters-stack #(pop @wrong-characters-stack)))
+
+(defn- clear-wrong-characters-stack! []
+  (swap! wrong-characters-stack (fn[n] [])))
+
+(defn- add-typed-character [character correct]
+  (swap! typed-characters #(conj @typed-characters (TypedCharacter. character correct))))
+
+(defn- keypress-watcher [_ _ _ key-code]
+  (let [character (keypress/key-code-to-character key-code)
+        cm @character-maps
         character-id (:id (next-character-id-to-type cm))
-        character-map (get cm character-id)]
-    (if (= new-val (:character character-map)) ; Value is typed right?
-      update-latest-character-map-as-typed
-      update-wrong-typed-characters)))
+        character-map (get cm character-id)
+        wrong-characters-stack-size (count @wrong-characters-stack)]
+    (if (= key-code 8) ; backspace handling
+      (let [stack-size (count @wrong-characters-stack)]
+        (if (> stack-size 0)
+          (remove-last-characters-stack!)
+          (update-latest-character-map-as-not-typed!)))
+      (let [correct (and (= character (:character character-map)) (= wrong-characters-stack-size 0))]
+        (when correct
+          (update-latest-character-map-as-typed!)
+          (clear-wrong-characters-stack!))
+        (when (not correct)
+          (add-wrong-character-to-stack character))
+        (add-typed-character character correct)))))
 
 (defn- setup! []
   (keypress/hook-keypress-detection!)
@@ -43,8 +74,7 @@
   (setup!)
   (into
     [:div#typing-area]
-    (map (fn [c]
-         [:span
-          {:style {:color (if (= (:typed c) false) "red" "black")}}
-          (:character c)])
-      @character-maps)))
+    (conj
+          (map (fn [c] (when (not (:typed c)) [:span {:style {:color "black"}} (:character c)])) @character-maps)
+          (map (fn [c] [:span {:style {:color "red"}} c]) @wrong-characters-stack)
+          (map (fn [c] (when (:typed c) [:span {:style {:color "blue"}} (:character c)])) @character-maps))))

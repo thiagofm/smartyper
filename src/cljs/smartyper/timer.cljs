d16bce88fa9b2dc6f5c4cba88662a13d472ad780
Thiago Fernandes Massa <thiagown@gmail.com>
2015-06-29 13:02:35 +0200

    Added net wpm formula

--- src/cljs/smartyper/timer.cljs
+++ src/cljs/smartyper/timer.cljs
@@ -5,6 +5,7 @@
 (def current-time (atom 0))
 (def start-time (atom 0))
 (def amount-of-typed-characters (atom 0))
+(def amount-of-errors (atom 0))
 
 (defn current-time-in-seconds []
   (.getTime (js/Date.)))
@@ -12,8 +13,12 @@
 (defn update-current-time []
   (swap! current-time #(current-time-in-seconds)))
 
+(defn compute-amount-of-errors [typed-characters]
+  (count (filter #(= (:correct %) false) typed-characters)))
+
 (defn- characters-being-typed [_ _ _ typed-characters]
   (swap! amount-of-typed-characters #(count typed-characters))
+  (swap! amount-of-errors #(compute-amount-of-errors typed-characters))
   (when (= 0 @start-time)
     (swap! start-time #(current-time-in-seconds))))
 
@@ -25,4 +30,9 @@
    [:p (if (not= @start-time 0) (int (/ (- @current-time @start-time) 1000)) 0)]
    [:p (if (not= @start-time 0)
          (int (* (/ (/ @amount-of-typed-characters 5) (/ (- @current-time @start-time) 1000)) 60))
+         0)]
+   [:p (if (not= @start-time 0)
+         (-
+           (int (* (/ (/ @amount-of-typed-characters 5) (/ (- @current-time @start-time) 1000)) 60))
+           (int (* (/ (/ @amount-of-errors 5) (/ (- @current-time @start-time) 1000)) 60)))
          0)]])
